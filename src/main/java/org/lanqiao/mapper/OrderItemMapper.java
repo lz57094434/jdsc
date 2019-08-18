82bf1c9843545a0fc125e81b6121b6cc1a9e0972
wufaxiayudeyun <2604308591@qq.com>
2019-08-18 15:00:10 +0800

    支付第一次

---
+++ src/main/java/org/lanqiao/mapper/OrderItemMapper.java
@@ -0,0 +1,25 @@
+package org.lanqiao.mapper;
+
+import org.lanqiao.entity.OrderItem;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer orderitemId);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer orderitemId);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+
+    List<OrderItem> Get();
+
+    int updateitem();
+}
